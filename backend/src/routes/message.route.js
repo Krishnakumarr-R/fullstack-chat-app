0cf63e68504105dc01effdf2190f8fa58378ee56
krishna kumar <krishna462003@gmail.com>
2025-07-01 13:59:46 +0530

    Serve frontend in production

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,11 @@
-import exppress from "express";
+import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getMessages,
   getUserForSidebar,
   sendMessage,
 } from "../controller/message.controller.js";
-const router = exppress.Router();
+const router = express.Router();
 
 router.get("/users", protectRoute, getUserForSidebar);
 router.get("/:id", protectRoute, getMessages);
